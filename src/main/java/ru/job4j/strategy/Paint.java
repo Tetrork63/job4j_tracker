1c704b22640798c9c3a14d04d45abfec5c3cb4af
UserDefault <1654466@gmail.com>
2022-11-04 21:25:08 +0300

    7. Шаблон проектирования - Стратегия [#786]

--- src/main/java/ru/job4j/strategy/Paint.java
+++ src/main/java/ru/job4j/strategy/Paint.java
@@ -4,6 +4,7 @@ public class Paint {
     public void draw(Shape shape) {
         System.out.print(shape.draw());
     }
+
     public static void main(String[] args) {
         Paint context = new Paint();
         context.draw(new Triangle());

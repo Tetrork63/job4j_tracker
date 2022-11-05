66361e05099204938f319e52a687fd48a9713efe
UserDefault <1654466@gmail.com>
2022-11-05 21:50:09 +0300

    2.1. Тест на ValidateInput [#311887]

---
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,56 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ValidateInputTest {
+
+    @Test
+    public void whenInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Menu:");
+        assertThat(selected).isEqualTo(1);
+    }
+
+    @Test
+    public void whenMultipleValidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1", "2", "3"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Menu:");
+        int selectedTwo = input.askInt("Menu:");
+        int selectedThree = input.askInt("Menu:");
+        assertThat(selected).isEqualTo(1);
+        assertThat(selectedTwo).isEqualTo(2);
+        assertThat(selectedThree).isEqualTo(3);
+    }
+
+    @Test
+    public void whenValidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Menu:");
+        assertThat(selected).isEqualTo(1);
+    }
+
+    @Test
+    public void whenValidInputNegativeNumber() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"-2"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Menu:");
+        assertThat(selected).isEqualTo(-2);
+    }
+
+}
